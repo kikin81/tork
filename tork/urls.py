a2c0cda7ac536a9bc78147f617d554ce5053635a
Francisco Velazquez <kikin81@gmail.com>
2014-07-29 23:51:08 -0700

    Added a "catch all" url endpoint for flatpages.

--- tork/urls.py
+++ tork/urls.py
@@ -11,3 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^upload/', include('torkapp.urls')),
 )
+
+urlpatterns += patterns('django.contrib.flatpages.views',
+    (r'^(?P<url>.*/)$', 'flatpage'),
+)
