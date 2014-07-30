15597bd50e8bdd68bc5d43fd4ad01fdfb238e940
Victor Lara <lara.m.victor@gmail.com>
2014-07-29 22:13:07 -0700

    removed useless code

--- torkapp/views.py
+++ torkapp/views.py
@@ -4,9 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	for key in request.POST:
-		value = request.POST[key]
-	# loop through keys and values
-	for key, value in request.POST.iteritems():
-		print "Key " + key + " value: " + value
 	return HttpResponse("OK!")
