d655120d54da27a3cb481a4844faf8159b8412ec
ivan <leo.ivan.aguayo@gmail.com>
2014-09-06 03:23:22 -0700

    Added Trip Lists

--- tork/src/js/app.js
+++ tork/src/js/app.js
@@ -4,15 +4,15 @@ app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
         when('/list', {
-            templateUrl: 'static/partials/session-list.html'
-            // controller: 'PhoneListCtrl'
+            templateUrl: 'static/partials/session-list.html',
+            controller: 'TripListCtrl'
         }).
         when('/item/:itemId', {
             templateUrl: 'static/partials/item-view.html',
             controller: 'CarSessionCtrl'
         }).
         otherwise({
-            redirectTo: '/item/0'
+            redirectTo: '/list'
         });
     }
 ]);
@@ -83,26 +83,38 @@ app.directive('d3Plot', function() {
             var barWidth = 40;
             var width = (barWidth * 3) * pdata.length;
             var height = 100;
-            var x = d3.time.scale().range(pdata[0][0], pdata[pdata.length-1][0]);
-            var y = d3.scale.linear().domain([0, d3.max(pdata, function(datum, index){return pdata[index][1];})])
-                    .rangeRound([0,height]);
+            var x = d3.time.scale().range(pdata[0][0], pdata[pdata.length - 1][0]);
+            var y = d3.scale.linear().domain([0, d3.max(pdata, function(datum, index) {
+                    return pdata[index][1];
+                })])
+                .rangeRound([0, height]);
             var graph = d3.select(elem[0])
                 .append('svg:svg').attr('width', width).attr('height', height);
             graph.selectAll("div").data(pdata).enter().append('svg:rect')
-                .attr('x', function(datum, index){return index * (barWidth * 2)})
-                .attr('y', function(datum){return height - datum[1];})
-                .attr('height', function(datum){return datum[1]})
+                .attr('x', function(datum, index) {
+                    return index * (barWidth * 2)
+                })
+                .attr('y', function(datum) {
+                    return height - datum[1];
+                })
+                .attr('height', function(datum) {
+                    return datum[1]
+                })
                 .attr('width', barWidth)
                 .attr('fill', '#5E778D');
             graph.selectAll("text").data(pdata).enter().append('svg:text')
-                .attr('x', function(datum, index){return index * (barWidth * 2) + barWidth})
-                .attr('y', function(datum){return height - datum[1];})
-                .attr('dx', -barWidth/2)
+                .attr('x', function(datum, index) {
+                    return index * (barWidth * 2) + barWidth
+                })
+                .attr('y', function(datum) {
+                    return height - datum[1];
+                })
+                .attr('dx', -barWidth / 2)
                 .attr('dy', '1.2em')
                 .attr('text-anchor', 'middle')
-                .text(function(datum){
-                    var s =  datum[1].split('.');
-                    return s[0] + '.'+ s[1].substring(0,1);
+                .text(function(datum) {
+                    var s = datum[1].split('.');
+                    return s[0] + '.' + s[1].substring(0, 1);
                 })
                 .attr('fill', '#FFF');
             scope.graph = graph;
@@ -114,7 +126,7 @@ app.controller('CarSessionCtrl', ['$scope',
     function($scope) {
         $scope.car = 'Subaru BRZ';
         $scope.id = '1010101';
-        $scope.day = '2-17-14';
+        $scope.date = '2-17-14';
 
         $scope.data = {
             '8/23/14 20:09:52.657': {
@@ -136,6 +148,32 @@ app.controller('CarSessionCtrl', ['$scope',
                 'Longitude': '-121.8012311',
                 'Latitude': '36.70291383',
                 'Speed': '68.97220612'
+            }
+        }
+        return;
+    }
+]);
+app.controller('TripListCtrl', ['$scope',
+    function($scope) {
+        $scope.trips = {
+            1: {
+                id: 0,
+                date: '28-Jul-2014 17:10',
+                vehicle: 'wrx',
+                readings: 748
+
+            },
+            2: {
+                id: 1,
+                date: '29-Jul-2014 7:11',
+                vehicle: 'wrx',
+                readings: 78
+            },
+            3: {
+                id: 2,
+                date: '30-Jul-2014 2:15',
+                vehicle: 'wrx',
+                readings: 438
             }
         }
         return;
